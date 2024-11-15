90fbc1a4c3f78ea298c9c55f613a0e000245e3de
headlessNode <harriskhan047@outlook.com>
2024-11-15 20:59:45 +0500

    feat: add logout notice for registering new account

--- routes/registerRouter.js
+++ routes/registerRouter.js
@@ -2,7 +2,13 @@ const {Router} = require('express');
 const registerRouter = Router();
 const registerController = require('../controllers/registerController.js');
 
-registerRouter.get('/', registerController.renderForm);
+registerRouter.get('/', (req, res) => {
+    if(req.isAuthenticated()) {
+        res.render('register', {user: res.locals.currentUser, isAuthenticated: true});
+    } else {
+        registerController.renderForm(req, res);
+    }
+});
 
 registerRouter.post('/', registerController.registerUser);
 
