5a368ff58bd60d403daf5d9193d03b37c67d5610
headlessNode <harriskhan047@outlook.com>
2024-11-15 20:52:28 +0500

    feat: add logout notice for switching accounts

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -2,7 +2,13 @@ const {Router} = require('express');
 const loginRouter = Router();
 const loginController = require('../controllers/loginController.js');
 
-loginRouter.get('/', loginController.renderForm);
+loginRouter.get('/', (req, res) => {
+    if(req.isAuthenticated()) {
+        res.render('login', {user: res.locals.currentUser, isAuthenticated: true});
+    } else {
+        loginController.renderForm(req, res);
+    }
+});
 loginRouter.post('/', loginController.loginUser);
 
 module.exports = loginRouter;
